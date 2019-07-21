1d9ed2c02842629b03d95cd784e91075e017e134
Hyo Gyeong <cdkrcd8@gmail.com>
2019-07-21 19:19:48 +0900

    멋사중앙과제 자기소개, 각오 및 아이디어소개

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+# Create your views here.
+def index(request):
+    blogs = Blog.objects
+
+    return render(request, 'index.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+
+    return render(request, 'detail.html', {'blog':blog_detail})
